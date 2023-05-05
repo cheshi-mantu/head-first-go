daa97fd378d0c0a904b2d750de1a1528f6c2d5d2
Egor Ivanov <cheshi.mantu@protonmail.com>
2023-05-05 20:02:25 +0300

    initial commin for chapter 2

---
+++ 002_manage/main.go
@@ -0,0 +1,25 @@
+package main
+
+// getting user's input, processing and giving the result
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	name := os.Getenv("USER")
+	fmt.Println("Hello from Go", name)
+
+	var now time.Time = time.Now()
+	var year int = now.Year()
+
+	fmt.Println(year)
+
+	wrongString := "This #s s#ck!"
+	replacer := strings.NewReplacer("#", "i")
+	fixed := replacer.Replace(wrongString)
+	fmt.Println("wrong content of", wrongString, "is replaced with", fixed)
+}
