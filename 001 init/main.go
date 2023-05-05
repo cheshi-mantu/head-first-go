51a77fdf0296ed269fe5fe7eeb4f2e2051c9e03b
Egor Ivanov <cheshi.mantu@protonmail.com>
2023-05-05 19:35:19 +0300

    added time used time

--- 001 init/main.go
+++ 001 init/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello, Go!")
 	name := os.Getenv("USER")
-	fmt.Println("Hello, from Go %s", name)
+	fmt.Println("Hello from Go %s", name)
+
+	var now time.Time = time.Now()
+	var year int = now.Year()
+
+	fmt.Println(year)
 }
