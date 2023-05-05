b49c9b3c61fbdb29c8d92aa375e4ea4361cf80a8
Egor Ivanov <cheshi.mantu@protonmail.com>
2023-05-05 19:39:50 +0300

    added strings used strings

--- 001 init/main.go
+++ 001 init/main.go
@@ -3,15 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 )
 
 func main() {
 	name := os.Getenv("USER")
-	fmt.Println("Hello from Go %s", name)
+	fmt.Println("Hello from Go", name)
 
 	var now time.Time = time.Now()
 	var year int = now.Year()
 
 	fmt.Println(year)
+
+	wrongString := "This #s s#ck!"
+	replacer := strings.NewReplacer("#", "i")
+	fixed := replacer.Replace(wrongString)
+	fmt.Println("wrong content of", wrongString, "is replaced with", fixed)
 }
