cbf7b7d2314886e619051ad79b750aaba781b165
Egor Ivanov <cheshi.mantu@protonmail.com>
2023-05-13 13:12:27 +0300

    adds the fileinfo.go

---
+++ 003_guess/fileinfo.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+//fileinfo shows the size of fileinfo.go file
+
+func main() {
+	fileinfo, err := os.Stat("fileinfo.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("file ", fileinfo.Name(), "size is", fileinfo.Size())
+
+}
